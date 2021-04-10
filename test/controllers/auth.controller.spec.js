e665dda257f4e9b69c20e4d274c43ed9420e6e54
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-10 17:32:16 +0530

    👌 IMPROVE: use beforeEach assertion

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -2,20 +2,26 @@ var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
 describe('AuthController', function () {
+  beforeEach(function () {
+    console.log('running before each');
+    authController.setRoles(['user']);
+  });
+
   describe('isAuthorized', function () {
     it('should return false if not authorized', function () {
-      assert.strictEqual(false, authController.isAuthorized(['user'], 'admin'));
+      assert.strictEqual(false, authController.isAuthorized('admin'));
     });
 
     it('should return true if authorized', function () {
-      assert.strictEqual(true, authController.isAuthorized(['user', 'admin'], 'admin'));
+      authController.setRoles(['user', 'admin']);
+      assert.strictEqual(true, authController.isAuthorized('admin'));
     });
   });
 
   describe('isAuthorizedAsync', function () {
     it('Should return false if not authorized', function (done) {
       this.timeout(2500);
-      authController.isAuthorizedAsync(['admin'], 'user', function (isAuth) {
+      authController.isAuthorizedAsync('user', function (isAuth) {
         assert.strictEqual(false, isAuth);
         done();
       });
@@ -23,7 +29,8 @@ describe('AuthController', function () {
 
     it('Should return true if authorized', function (done) {
       this.timeout(2500);
-      authController.isAuthorizedAsync(['admin', 'user'], 'user', function (isAuth) {
+      authController.setRoles(['admin', 'user']);
+      authController.isAuthorizedAsync('user', function (isAuth) {
         assert.strictEqual(true, isAuth);
         done();
       });
