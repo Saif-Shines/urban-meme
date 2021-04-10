cb4862c6919faf600eefa10e6b573c2b5ab7bba9
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-10 17:53:07 +0530

    👌 IMPROVE: ability to writing pending test cases

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
 describe('AuthController', function () {
-  beforeEach(function () {
+  beforeEach(function settingUpRoles() {
     console.log('running before each');
     authController.setRoles(['user']);
   });
@@ -16,12 +16,15 @@ describe('AuthController', function () {
       authController.setRoles(['user', 'admin']);
       assert.strictEqual(true, authController.isAuthorized('admin'));
     });
+
+    it('should not allow a get if not authorized');
+    it('should allow get if authorized');
   });
 
   describe('isAuthorizedAsync', function () {
     it('Should return false if not authorized', function (done) {
       this.timeout(2500);
-      authController.isAuthorizedAsync('user', function (isAuth) {
+      authController.isAuthorizedAsync('admin', function (isAuth) {
         assert.strictEqual(false, isAuth);
         done();
       });
